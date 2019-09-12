1c3b3da9daa140f6df731df60309a1e6072e80e6
noracyrd <norabyrd@alumni.upenn.edu>
2019-09-12 00:48:22 -0500

    added question two and managed to sort out hiding/showing on click functions!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,17 +4,55 @@ var wrongCounter = 0;
 // Placeholder for how long they have to answer the question
 var jeopardy;
 // Placeholder for timer to move to the next question (whether they got it right or wrong)
-var nextQuestion;
+var nextQuestion = function(question1,question2){
+    $(question1).hide();
+    $(question2).show();
+}
+
+
+
+$("#question1").hide();
+$("#question2").hide();
 
 //reference for later:     $("p").hide();
+//notes: am able to hide elements with jquery! so should be able to include hide on the timer!!
+//will have to figure out how to keep them hidden when the game starts, maybe they are all hidden at the start or something
+//might have to be more specific for onclicks - thinking each question box needs separate identifiers cuz right now anything wrong is going wrong if you click one wrong
 
-$( document ).ready(function() {
+$("#start").on("click", function () {
+    $("#thequestion").html("#question1");
+    $("#question1").show();
+    
+  });
 
     //buttons clicked right or wrong
-$(".wrongAnswer").on("click", function (){
-    $(".wrongAnswer").css("background-color","red");
-    $("#correctAnswer").css("background-color","green");
+$(".wrongAnswer1").on("click", function (){
+    $(".wrongAnswer1").css("background-color","red");
+    $("#correctAnswer1").css("background-color","green");
+    wrongCounter++;
+    setTimeout(nextQuestion,5000,"#question1","#question2");
+    
 });
+
+$("#correctAnswer1").on("click", function (){
+    $(".wrongAnswer1").css("background-color","red");
+    $("#correctAnswer1").css("background-color","green");
+    rightCounter++;
+});
+
+$(".wrongAnswer2").on("click", function (){
+    $(".wrongAnswer2").css("background-color","red");
+    $("#correctAnswer2").css("background-color","green");
+    wrongCounter++;
+});
+
+$("#correctAnswer2").on("click", function (){
+    $(".wrongAnswer2").css("background-color","red");
+    $("#correctAnswer2").css("background-color","green");
+    rightCounter++;
+});
+
+
 
 
   
@@ -29,4 +67,3 @@ $(".wrongAnswer").on("click", function (){
 
 
 
-});
